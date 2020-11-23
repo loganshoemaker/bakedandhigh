a0409e98631bae370fe9565db4ca01d950f899ef
Logan Shoemaker <logansshoemaker@gmail.com>
2020-11-22 19:56:40 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,35 @@ const half = "½";
 const quarter = "¼";
 const threeFourths = "¾";
 
+const containsFraction = (text) => {
+    return (
+        text.includes(half) ||
+        text.includes(quarter) ||
+        text.includes(threeFourths)
+    );
+};
+
 var main = () => {
     var ingredients = Array.from(document.getElementsByClassName("ingredients-item-name"));
     ingredients.forEach(ingredient => {
         const text = ingredient.textContent.trim().toLowerCase();
-        const wholeAmount = text.replace(/\D/g, "");
+        
+        let wholeAmount = text.replace(/\D/g, "");
+        
+        if(wholeAmount == "") {
+            if(containsFraction(text)) {
+                let update = "";
+
+                if(text.includes("flour")) {
+                    update = "You may want to add up to 2 tablespoons for every 1 cup!";    
+                }
+                if(text.includes("sugar")) {
+                    update = "You may want to remove up to 2 tablespoons of sugar for every 1 cup!";
+                }
+
+                ingredient.innerHTML = `${text} : <span class="habk-wrapper">${update}</span>`;
+            }
+        }
 
         if(
             Number.isInteger(parseInt(wholeAmount)) &&
