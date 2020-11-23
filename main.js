e071f13dd9ce825b83e7b5a360d8f6b45cba0a90
Logan Shoemaker <logansshoemaker@gmail.com>
2020-11-22 18:29:04 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-var updateContent = () => {
+const half = "½";
+const quarter = "¼";
+const threeFourths = "¾";
 
+var main = () => {
+    var ingredients = Array.from(document.getElementsByClassName("ingredients-item-name"));
+    ingredients.forEach(ingredient => {
+        const text = ingredient.textContent.trim();
+        console.log(text);
+        console.log("contains word cup: ", text.includes("cup"));
+        console.log("contains 1/2: ", text.includes("½"));
+        console.log(Array.from(text));
+        
+        Array.from(text).forEach(character => {
+            console.log(character, character === half);
+            console.log(character, character === quarter);
+            console.log(character, character === threeFourths);
+        });
+    });
 }
 
-setTimeout(function(){ updateContent()}, 1000);
+setTimeout(function(){ main()}, 1000);
