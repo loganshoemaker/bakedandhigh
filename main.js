f5aa45c8be87d849c5155413f35a7f335428fb08
Logan Shoemaker <logansshoemaker@gmail.com>
2020-11-22 19:26:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ var main = () => {
             text.includes("cup") &&
             text.includes("flour")
         ){
-            const update = `You may want to add up to ${parseInt(wholeAmount) * 2} tablespoons of flour!`;
+            const update = `You may want to add up to ${parseInt(wholeAmount) * 2} tablespoon${wholeAmount === "1" ? "" : "s"} of flour!`;
             ingredient.innerHTML = `${text} : <span class="baked-and-high-wrapper">${update}</span>`;
         }
 
@@ -23,7 +23,7 @@ var main = () => {
             text.includes("cup") &&
             text.includes("sugar")
         ){
-            const update = `You may want to remove up to ${parseInt(wholeAmount)} tablespoon${wholeAmount === "1" ? "" : "s"} of this sugar!`;
+            const update = `You may want to remove up to ${parseInt(wholeAmount) * 2} tablespoon${wholeAmount === "1" ? "" : "s"} of this sugar!`;
             ingredient.innerHTML = `${text} : <span class="baked-and-high-wrapper">${update}</span>`;
         }
 
@@ -33,6 +33,20 @@ var main = () => {
         ){
             const update = `You may want to start with only 75% of the recommended amount of yeast!`;
             ingredient.innerHTML = `${text} : <span class="baked-and-high-wrapper">${update}</span>`;
+        }
+
+        if(
+            Number.isInteger(parseInt(wholeAmount)) &&
+            text.includes("egg") && 
+            (
+                !text.includes("extra large")
+            )
+        ){
+            const text = ingredient.innerHTML.replace("egg", "extra large egg");
+
+
+            const update = `You may want to start with only 75% of the recommended amount of yeast!`;
+            ingredient.innerHTML = text;
         }
 
         if(
@@ -50,7 +64,7 @@ var main = () => {
         {
             const update = `You may want to use half the amount of this chemical leavener!`;
             console.log(update)
-            ingredient.innerHTML = `${text} : <span class="baked-and-high-wrapper">${update}</span>`;
+            ingredient.innerHTML = `${text}<span class="baked-and-high-wrapper">${update}</span>`;
         }
     });
 }
